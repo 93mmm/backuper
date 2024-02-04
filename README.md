a8e2cceeaf4ba0bba34be58b910a122fda6ded0a
93mmm <a@a.local>
2024-02-04 17:51:22 +0300

    bash sc edit

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # backuper
 backuper for my mc hardcore folders
+
+# Build and run project:
+```bash
+chmod +x build.sh
+./build.sh release
+```
+
+#### Todo:
+- build.sh install dependencies
+- README.md list dependencies
--- compile.sh
+++ compile.sh
@@ -6,42 +6,49 @@ run_binary="rbin"
 clean="clean"
 export_compile_commands="ecc"
 
+execute_script() {
+  if [ $action = $debug ] ; then
+    cmake -B build -S . -DCMAKE_BUILD_TYPE=Debug && cmake --build build
+    if [ $? = 0 ]; then
+      clear
+      ./build/Backuper.app/Contents/MacOS/Backuper
+    fi
 
-echo "Possible actions:"
-echo "  \"$debug\": build debug project version"
-echo "  \"$release\": build release project version"
-echo "  \"$run_binary\": runs binary application"
-echo "  \"$clean\": cleans project"
-echo "  \"$export_compile_commands\": exports compile_commands.json for your clangd"
+  elif [ $action = $release ]; then
+    cmake -B build -S . -DCMAKE_BUILD_TYPE=Release && cmake --build build
+    if [ $? = 0 ]; then
+      clear
+      open build/Backuper.app
+    fi
 
-read -p "What do you want to do? " action
-
-if [ $action = $debug ] ; then
-  cmake -B build -S . -DCMAKE_BUILD_TYPE=Debug && cmake --build build
-  if [ $? = 0 ]; then
-    clear
+  elif [ $action = $run_binary ]; then
     ./build/Backuper.app/Contents/MacOS/Backuper
-  fi
-
-elif [ $action = $release ]; then
-  cmake -B build -S . -DCMAKE_BUILD_TYPE=Release && cmake --build build
-  if [ $? = 0 ]; then
     clear
-    open build/Backuper.app
+
+  elif [ $action = $clean ]; then
+    rm -rf build
+    rm compile_commands.json
+    echo "Everything is cleaned up"
+
+  elif [ $action = $export_compile_commands ]; then
+    mv build/compile_commands.json .
+
+  else
+    echo "Wrong input"
   fi
+}
 
-elif [ $action = $run_binary ]; then
-  ./build/Backuper.app/Contents/MacOS/Backuper
-  clear
-
-elif [ $action = $clean ]; then
-  rm -rf build
-  rm compile_commands.json
-  echo "Everything is cleaned up"
-
-elif [ $action = $export_compile_commands ]; then
-  mv build/compile_commands.json .
-
+if [[ $# -gt 0 ]]; then
+  action=$1
 else
-  echo "Wrong input"
+  echo "Possible actions:"
+  echo "  \"$debug\": build debug project version"
+  echo "  \"$release\": build release project version"
+  echo "  \"$run_binary\": runs binary application"
+  echo "  \"$clean\": cleans project"
+  echo "  \"$export_compile_commands\": exports compile_commands.json for your clangd"
+
+  read -p "What do you want to do? " action
 fi
+
+(execute_script)
